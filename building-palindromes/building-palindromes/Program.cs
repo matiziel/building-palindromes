1df091d2f5e79ab5a4c6ca6434b333a677ee3a1c
Mateusz <matiziel10@gmail.com>
2019-11-17 21:26:36 +0100

    First commit

---
+++ building-palindromes/building-palindromes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace building_palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
